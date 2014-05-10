a1c4d3c58e5881eb50e8e93855e27d7c708ab499
CarrMar14 <you@example.commarcusrcarroll@gmail.com>
2014-05-10 02:03:18 +0000

    I completed Lesson 4 and then made sure I could see it in the web browser.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
-Marcuster::Application.routes.draw do
+Marcuster::Application.routes.draw do 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+root 'places#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
@@ -53,4 +53,5 @@ Marcuster::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  
 end

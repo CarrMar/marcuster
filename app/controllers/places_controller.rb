85b481586b2fe8ae955f42d5e0d81b88f7243f6a
CarrMar14 <you@example.commarcusrcarroll@gmail.com>
2014-05-29 17:11:09 +0000

    added form data and made form clear map

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -20,8 +20,9 @@ def new
   end
 
 def show
-    @place = Place.find(params[:id])  
-  end
+  @place = Place.find(params[:id])
+  @comment = Comment.new
+end
 
 def edit
     @place = Place.find(params[:id])
